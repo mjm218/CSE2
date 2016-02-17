87cb207f8009360b444f904362962901e94b3657
mjm218 <mjm218@lehigh.edu>
2016-02-17 03:20:32 +0000

    hw03-Convert

--- hw03-Convert/Convert.java
+++ hw03-Convert/Convert.java
@@ -1,12 +1,12 @@
 //Michael Mcateer
 //hw03 cse 2 - convert meters to inches
 import java.util.Scanner;
-public class Convert{
-    public static void main(String[] args) {
+public class Convert{ //start of class
+    public static void main(String[] args) { //start of method
     Scanner myScanner = new Scanner(System.in); //declare scanner
-    System.out.println("distance in meters:");
-    double meters=myScanner.nextDouble();
-    double inches=meters*39.3701;
-    System.out.println(inches + " inches is " + meters + " meters");
+    System.out.println("distance in meters:"); //insert distance meters
+    double meters=myScanner.nextDouble(); //declare meters
+    double inches=meters*39.3701; //1 meter = 39.3701
+    System.out.println(inches + " inches is " + meters + " meters"); //inches to meters conversion
     }
 }
--- hw03-Pyramid/Pyramid.java
+++ hw03-Pyramid/Pyramid.java
@@ -1,16 +1,16 @@
 //Michael McAteer
 //hw03- volume of a pyramid
-import java.util.Scanner;
-public class Pyramid{
-    public static void main(String[] args){
-    Scanner myScanner = new Scanner(System.in);
-    System.out.println("length of pyramid square:");
-    double length = myScanner.nextDouble();
-    System.out.println("length of pyramid square:");
-    double width = myScanner.nextDouble();
-    System.out.println("height of pyramid square:");
-    double height = myScanner.nextDouble();
-    double volume= (length*width*height)/(3);
-    System.out.println(volume);
+import java.util.Scanner; //import scanner
+public class Pyramid{ //start of class
+    public static void main(String[] args){ //start of method
+    Scanner myScanner = new Scanner(System.in); //declare scanner
+    System.out.println("length of pyramid square:"); //input length of pyramid square
+    double length = myScanner.nextDouble(); //declare length variable
+    System.out.println("width of pyramid square:"); //input width of pyramid square
+    double width = myScanner.nextDouble(); //declare width variable
+    System.out.println("height of pyramid square:"); //input height of pyramid
+    double height = myScanner.nextDouble(); //deckare height variable
+    double volume= (length*width*height)/(3); //calculate volume
+    System.out.println(volume); //print volume
     }
 }
